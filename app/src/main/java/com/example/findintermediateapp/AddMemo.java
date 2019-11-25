9cf3d60d949d5511de2b5863c5cf22f9294182ec
Jaeyeon <wodus5677@gmail.com>
2019-11-25 17:06:44 +0900

    add memo bitmap array

--- app/src/main/java/com/example/findintermediateapp/AddMemo.java
+++ app/src/main/java/com/example/findintermediateapp/AddMemo.java
@@ -3,12 +3,18 @@ package com.example.findintermediateapp;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.InputStream;
 
 public class AddMemo extends AppCompatActivity {
     String addedLocation;
@@ -17,6 +23,13 @@ public class AddMemo extends AppCompatActivity {
     FeedReaderDbHelper dbHelper = new FeedReaderDbHelper(this);
     ImageView iv_memoImage1;
     ImageView iv_memoImage2;
+    private static final int REQUEST_CODE1 = 0;
+    private static final int REQUEST_CODE2 = 1;
+    public boolean add_memo_image1 = false;
+    public boolean add_memo_image2 = false;
+    private Uri filePath;
+    public int addCount = 0;
+    public static Bitmap[] bitmap_array;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +41,28 @@ public class AddMemo extends AppCompatActivity {
 //        tv_addedLocation.setText(addedLocation);
         iv_memoImage1 = findViewById(R.id.add_memo_image1);
         iv_memoImage2 = findViewById(R.id.add_memo_image2);
+
+        iv_memoImage1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent, REQUEST_CODE1);
+            }
+        });
+        iv_memoImage2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent, REQUEST_CODE2);
+            }
+        });
         btn_addMemo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 SQLiteDatabase db = dbHelper.getWritableDatabase();
                 ContentValues values = new ContentValues();
                 values.put(FeedReaderContract.FeedEntry.NAME, "울산대학교");
@@ -45,9 +76,37 @@ public class AddMemo extends AppCompatActivity {
 
                 Intent mainIntent = new Intent(AddMemo.this, MainActivity.class);
                 startActivity(mainIntent);
-
-
             }
         });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == REQUEST_CODE1) {
+            if(resultCode == RESULT_OK) {
+                if(add_memo_image1 == false) {
+                    addCount++;
+                    bitmap_array = new Bitmap[addCount];
+                    add_memo_image1 = true;
+                }
+                try {
+                    filePath = data.getData();
+                    InputStream in = getContentResolver().openInputStream(data.getData());
+                    bitmap_array[0] = BitmapFactory.decodeStream(in);
+                    in.close();
+                    iv_memoImage1.setImageBitmap(bitmap_array[0]);
+                }catch(Exception e)
+                {
+
+                }
+            }
+            else if(resultCode == RESULT_CANCELED)
+            {
+                Toast.makeText(this, "사진 선택 취소", Toast.LENGTH_LONG).show();
+            }
+        }
+        else if(requestCode == REQUEST_CODE2) {
+
+        }
     }
 }
